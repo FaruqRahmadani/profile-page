bc42a6a6b0a06327c8d4c9a0976514f46ad9312b
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-05-17 09:18:38 +0700

    perubahan favicon

--- dist/javascript/_easter.js
+++ dist/javascript/_easter.js
@@ -1,6 +1,6 @@
 $(document).keypress("a", function(e){
   if(e.ctrlKey){
-    $("#touch-text").html("you found an easter egg maybe?")
+    $("#touch-text").html("❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️")
     $(".jumbotron, .touch-icon").attr("style", "color: red !important;")
     $(".touch-list").append(
       '<a href="http://wa.me/6289601012200" target="_blank" class="touch-icon"><i class="fa fa-2x fa-whatsapp"></i></a>'
